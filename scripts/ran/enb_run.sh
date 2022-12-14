fab3eb6792768733f953973b7286aa0281aabfdb
JulesPatmanidis <ipat@hotmail.gr>
2022-12-14 19:36:01 +0000

    Configure 2nd eNB

--- scripts/ran/enb_run.sh
+++ scripts/ran/enb_run.sh
@@ -14,4 +14,4 @@ cd cmake_targets/
 # --log_config.global_log_options level,nocolor,time,thread_id --log_config.global_log_level debug | tee ~/logs/eNB1.log 2>&1 &
 
 
-sudo -E ./ran_build/build/lte-softmodem -O /local/repository/config/ran/enb.conf --emulate-l1 --nsa --log_config.global_log_options level,nocolor,time,thread_id | tee eNB.log 2>&1
+sudo -E ./ran_build/build/lte-softmodem -O /local/repository/config/ran/enb.conf --emulate-l1 --node_numer 1 --log_config.global_log_options level,nocolor,time,thread_id | tee eNB.log 2>&1
--- scripts/ran/enb_run2.sh
+++ scripts/ran/enb_run2.sh
@@ -9,4 +9,4 @@ cd /local/repository/openairinterface5g/
 source oaienv
 cd cmake_targets/
 
-sudo -E ./ran_build/build/lte-softmodem -O /local/repository/config/ran/enb2.conf --emulate-l1 --nsa --log_config.global_log_options level,nocolor,time,thread_id | tee eNB.log 2>&1
+sudo -E ./ran_build/build/lte-softmodem -O /local/repository/config/ran/enb2.conf --emulate-l1 --node_numer 1 --log_config.global_log_options level,nocolor,time,thread_id | tee eNB.log 2>&1
